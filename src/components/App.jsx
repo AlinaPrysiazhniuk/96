4526b99fa813c2078db096fd2671268ee726b466
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-19 19:41:14 +0200

    user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,26 @@ import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import { FriendsList } from "./FriendList/FriendsList";
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
-import user from "user.json";
+import user from "/src/user.json";
 import data from "data.json";
 import friends from "friends.json";
 import transactions from "transactions.json";
 
-
-
 export const App = () => {
   return (
     <>
-      <Profile 
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    followers={user.stats.followers}
-    views={user.stats.views}
-    likes={user.stats.likes}
-    />
-<Statistics title="Upload stats" stats={data} />
-<FriendsList friends={friends}/>
-<TransactionHistory items={transactions} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendsList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
